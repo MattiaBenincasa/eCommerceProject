f00fc4fd70cc5772e5e1e8ab802c4ce737b2f92b
MattiaBenincasa <mattia.benincasa03@gmail.com>
2025-06-15 10:33:52 +0200

    Added admin function to add a new user

--- django_project/accounts/admin.py
+++ django_project/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import CustomUser
 
 # Register your models here.
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('birth_date', 'country', 'city', 'address', 'postcode')}),
+    )
+    list_display = UserAdmin.list_display + ('birth_date', 'country', 'city', 'address', 'postcode')
+    list_filter = UserAdmin.list_filter + ('country', 'city')
+    search_fields = UserAdmin.search_fields + ('country', 'city', 'address')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
