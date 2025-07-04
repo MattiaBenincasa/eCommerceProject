7e2499b294a18e070e9d9d895e04cc94021828c6
MattiaBenincasa <mattia.benincasa03@gmail.com>
2025-07-04 19:33:13 +0200

    Fixed whitenoise configuration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -129,9 +129,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
